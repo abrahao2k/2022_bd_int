87b1c7c6442b63c5350a88b1a0bb6ff7cdb148d0
abrahao2k <117474324+abrahao2k@users.noreply.github.com>
2022-11-18 14:57:01 -0300

    Completo Completo :)

--- completo.py
+++ completo.py
@@ -27,14 +27,63 @@ def pesquisar():
             print("----------------------------------")
         print(f"{len(dados)} Resultados encontrados. \n")
 
+def alterar():
+    print("=== ALTERAÇÃO DE DADOS DO PRODUTO ===")
+    codigo = input("Código do produto: ")
+    cur.execute("SELECT * FROM produto WHERE codigo = %s", (codigo,))
+    dados = cur.fetchall()
+    if len(dados) == 0:
+        print("Código não encontrado!\n")
+    else:
+        print(f"nome: {dados[0][1]}")
+        print(f"preco: {dados[0][2]}")
+        print(f"estoque: {dados[0][3]}")
+        
+        coluna = input("Qual coluna deseja alterar? ")
+        
+        if coluna in ('nome', 'preco', 'estoque'):
+            valor  = input(f"Digite o novo {coluna}: ")
+            comando = "UPDATE produto SET " + coluna + " = %s WHERE codigo = %s"
+            cur.execute(comando, (valor, codigo) )
+            conexao.commit()
+            print("Atualizado com sucesso.\n")
+            input("")
+        else:
+            print("Coluna inválida!\n")
+
+
+def excluir():
+    print("=== EXCLUSÃO DE DADOS DO PRODUTO ===")
+    codigo = input("Código do produto: ")
+    cur.execute("SELECT * FROM produto WHERE codigo =  %s ", (codigo,))
+    dados = cur.fetchall()
+    if len(dados) == 0:
+        print("Código não encontrado!\n")
+    else:
+        print(f"nome: {dados[0][1]}")
+        print(f"preco: {dados[0][2]}")
+        print(f"estoque: {dados[0][3]}")
+        
+        resp = input("Confirma a exclusão? (s/n) ")
+        if resp == "s":
+            cur.execute("DELETE FROM produto WHERE codigo = %s ", (codigo,))
+            conexao.commit()
+            print("Excluído com sucesso.\n")
+    
+
+
 ### PROGRAMA PRINCIPAL ##########################################
 while True:
     print("=== MENU ===")
     print("1 Cadastrar")
     print("2 Pesquisar")
+    print("3 Alterar")
+    print("4 Excluir")
     opcao = input("Opção? ")
     
-    if opcao == "1" : cadastrar()
+    if   opcao == "1" : cadastrar()
     elif opcao == "2" : pesquisar()
+    elif opcao == "3" : alterar()
+    elif opcao == "4" : excluir()
     else: break
     
